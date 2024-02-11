ecd4c1861b3272a2c4e611b44698f4f1003edbe9
VIJAYAKUMARAN <106998045+VIJAYAKUMARAN03@users.noreply.github.com>
2024-02-11 17:40:20 +0530

    Updated Variables Name in Data-Annotation.py

--- Data Annotation/Data-Annotation.py
+++ Data Annotation/Data-Annotation.py
@@ -2,24 +2,24 @@ import pydicom
 import os
 import pandas as pd 
 
-des_path = './Datas/SEPARATED/'
+des_path_base = './Datas/SEPARATED/'
 CN =0
 MCI =0
 AD=0
 T=0
-dfile = pd.read_csv('./Datas/ADNI_MRI_8_18_2023.csv')
-l = len(dfile)
+csv_file = pd.read_csv('./Datas/ADNI_MRI_8_18_2023.csv')
+length = len(csv_file)
 basepath = './Datas/ADNI'
-for i in range(l):
-    path = basepath + '/' + dfile.iloc[i]['Subject']
-    type =  dfile.iloc[i]['Group']
-    for f in os.listdir(path):
-        if f!='SURVEY':
-            dir_path1 = path + '/' +  f
-            for f1 in os.listdir(dir_path1):
-                dir_path2 = dir_path1 + '/' +  f1
-                for f2 in os.listdir(dir_path2):
-                    dir_path3 = dir_path2 + '/' +  f2
+for i in range(length):
+    path = basepath + '/' + csv_file.iloc[i]['Subject']
+    type =  csv_file.iloc[i]['Group']
+    for dir1 in os.listdir(path):
+        if dir1!='SURVEY':
+            dir_path1 = path + '/' +  dir1
+            for dir2 in os.listdir(dir_path1):
+                dir_path2 = dir_path1 + '/' +  dir2
+                for dir3 in os.listdir(dir_path2):
+                    dir_path3 = dir_path2 + '/' +  dir3
                     for file in os.listdir(dir_path3):
                         if(type == 'CN'):
                             CN = CN +1
@@ -29,12 +29,12 @@ for i in range(l):
                             MCI = MCI +1
 
                         T = T +1
-                        dir_path4 = dir_path3 + '/' +  file
-                        final_path = des_path + type + '/' + file
-                        ds = pydicom.dcmread(dir_path4)
-                        ds.save_as(final_path)
+                        source_path = dir_path3 + '/' +  file
+                        des_path = des_path_base + type + '/' + file
+                        ds = pydicom.dcmread(source_path)
+                        ds.save_as(des_path)
 
-    print(i+1,"/",l)
+    print(i+1,"/",length)
 
 print('CN ',CN)
 print('AD ',AD)
